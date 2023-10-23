d97ee0e00825a6c3582eb5df1d15b23c776519cb
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2023-10-23 13:20:08 -0700

    Update BackgroundItemsLocator.sh

--- BackgroundItemsLocator.sh
+++ BackgroundItemsLocator.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-#set -x
+set -x
 
 # By default zsh exits the script if a wildcard search fails. Turn that off.
 unsetopt nomatch
@@ -7,39 +7,38 @@ unsetopt nomatch
 # We use PlistBuddy to read data in the LaunchD items
 pBuddy="/usr/libexec/PlistBuddy"
 
+# Column headers
+/bin/echo "Label,TeamID,Program,LaunchD Item"
+
 check_folder(){
     # $1 is a folder which contains LaunchD items
     for backgroundItem in "${1}"/*; do
         if [ -e "$backgroundItem" ]; then
-            # Separators for readability
-            echo "--"
-            echo ""
             # Provide the full file path
-            echo "Full path: $(realpath $backgroundItem)"
+            fullPath="$(realpath $backgroundItem)"
             # Read the LaunchD Label
-            echo "Label: $($pBuddy -c "Print Label" $backgroundItem)"
+            label="$($pBuddy -c "Print Label" $backgroundItem)"
             # Find the Program the LaunchD item is running.
             if $($pBuddy -c "Print Program" $backgroundItem  > /dev/null 2>&1) ; then
                 program=$($pBuddy -c "Print Program" $backgroundItem)
-                echo "Program: $program"
             # If there is no Program value, probably there are only ProgramArguments. The first entry in that array is the program thats being run.
             elif $($pBuddy -c "Print ProgramArguments" $backgroundItem  > /dev/null 2>&1); then
                 program=$($pBuddy -c "Print ProgramArguments:0" $backgroundItem)
-                echo "ProgramArguments: $program"
             # Something weird, the LaunchD item has no Program? May be edge case? May be impossible? Including anyhow...
             else
-                echo "No Program Identified"
+                program="null"
             fi
-        # Get the TeamID of the program
-        teamID=$(codesign -dv "$program" 2>&1 | grep 'TeamIdentifier' | awk -F '=' '{print $2}')
-        # If there is a teamID, report it
-        if [ ! -z "$teamID" ]; then
-            echo "TeamID: $teamID"
-        else
-            # No TeamID here means the item is not signed (like most scripts)
-            echo "TeamID: Unsigned"
+        if [ ! -e "$program" ]; then
+            program="ERROR: Program does not exist: $program"
         fi
-        echo ""
+        # Get the TeamID of the program
+        if ! teamID=$(codesign -dv "$program" 2>&1 | grep 'TeamIdentifier' | awk -F '=' '{print $2}'); then
+            teamID="null"
+        fi
+        if [[ "$teamID" == 'not set' ]] || [ -z "$teamID" ]; then
+            teamID="null"
+        fi
+        /bin/echo "$label,$teamID,$program,$fullPath"
         fi
     done
 }
